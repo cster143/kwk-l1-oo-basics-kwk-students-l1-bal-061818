ea2032f3e9bef24d75f302a9a20da299a41620ad
cster143 <cster143@gmail.com>
2018-06-22 15:32:54 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,14 +3,12 @@ class Book
 
 attr_accessor :author :page_count :genre
 attr_reader :title
-attr_writer :turn_page
+
 
   def initialize(title) #initializing
     @title= title
   end
-  def title #getter
-    @title
-  end
+
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
