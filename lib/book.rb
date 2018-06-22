6210ba92c29011805988fd77f4f8cafe41f98d5f
cster143 <cster143@gmail.com>
2018-06-22 15:16:22 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -31,4 +31,10 @@ class Book
   def genre #getter
     @genre
   end
+  def turn_page
+    puts "Flipping the page... wow, you read fast!"
+  end
 end
+
+Harry_Potter = Book.new ("Sorcerer's Stone")
+Harry_Potter.turn_page 
