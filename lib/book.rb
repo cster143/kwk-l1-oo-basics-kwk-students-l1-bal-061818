07d3320b19eb8fef497ed3bc2b9732719e58669c
cster143 <cster143@gmail.com>
2018-06-22 15:10:47 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,27 @@
 # book.rb
 # Add you Book class here
 class Book
-  def initialize(title)
+  def initialize(title) #initializing
     @title = title
   end
-  def title #getter 
+
+  def title #getter
     @title
+  end
+
+  def author = (author) #setter
+    @author = author
+  end
+
+  def author #getter
+    @author
+  end
+
+  def page_number = (pages)
+    @page_number = pages
+  end
+
+  def page_number
+    @page_number
   end
 end
